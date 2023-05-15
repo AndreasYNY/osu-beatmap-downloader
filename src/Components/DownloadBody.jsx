40e721ef2746e38ce8e722f6974b3e1f9f93b089
CookyNdi <yandikun145@gmail.com>
2023-05-15 15:52:42 +0700

    change to vite react and using tailwind

--- src/Components/DownloadBody.jsx
+++ src/Components/DownloadBody.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from "react";
-import { Card, Form, Button } from "react-bootstrap";
 
 const DownloadBody = () => {
   const choseLink = useRef(null);
@@ -45,33 +44,60 @@ const DownloadBody = () => {
     setBeatmapSetId(data);
   };
 
+
+  const handleHelp = () => {
+    window.open(`https://cdn.discordapp.com/attachments/712958280863055903/1107586308165095455/image.png`, "_blank");
+  };
+
   return (
-    <Card style={{ width: "500px" }}>
-      <Card.Body>
-        <Card.Title>Osu Beatmap Download Mirror</Card.Title>
-        <Form>
-          <Form.Group className="mb-3" controlId="inputId">
-            <Form.Label>Beatmapset Id</Form.Label>
-            <Form.Control
-              type="text"
-              aria-describedby="passwordHelpBlock"
-              placeholder="1910670"
-              onChange={getInputValue}
-              value={beatmapSetId}
-            />
-          </Form.Group>
-          <Form.Select aria-label="Default select example" ref={choseLink}>
+    <div className="w-full min-h-screen flex justify-center items-center bg-white dark:bg-slate-950">
+      <div
+        className="h-auto bg-slate-50 border-slate-600 dark:bg-slate-900 border-2 p-6 flex flex-col rounded-xl"
+        style={{ width: "420px" }}
+      >
+        <h1 className="text-2xl font-semibold text-center mb-2 dark:text-white">Alt Beatmap Downloader</h1>
+        <form className="flex flex-col">
+          <label className="text-lg font-normal mb-2 dark:text-white">Beatmapset Id</label>
+          <input
+            type="text"
+            className="p-3 rounded-lg mb-2 border-slate-600 border-2 dark:text-white dark:bg-slate-800"
+            placeholder="842412"
+            onChange={getInputValue}
+            value={beatmapSetId}
+          />
+          <label className="text-base font-normal mb-3 underline text-end cursor-pointer dark:text-white" onClick={() => handleHelp()}>
+            Where&apos;s Beatmap Set Id?
+          </label>
+          <select
+            className="w-full p-3 rounded-lg mb-3 border-slate-600 border-2 dark:text-white dark:bg-slate-800"
+            aria-label="Default select example"
+            ref={choseLink}
+          >
             <option value="1">Nerinyan</option>
             <option value="2">Nerinyan (No Video)</option>
             <option value="3">Chimu</option>
             <option value="4">Beatconnect</option>
-          </Form.Select>
-        </Form>
-        <Button variant="primary" className="mt-3" style={{ width: "100%" }} id="btn" onClick={() => handleDownload()}>
+          </select>
+        </form>
+        <button
+          className="p-3 rounded-lg bg-blue-500 text-white mb-3 dark:bg-blue-700"
+          style={{ width: "100%" }}
+          id="btn"
+          onClick={() => handleDownload()}
+        >
           Download
-        </Button>
-      </Card.Body>
-    </Card>
+        </button>
+        <img
+          className="w-full object-cover rounded-lg border-slate-600 border-2 dark:text-white"
+          src={
+            beatmapSetId !== ""
+              ? `https://assets.ppy.sh/beatmaps/${beatmapSetId}/covers/cover.jpg`
+              : "https://assets.ppy.sh/beatmaps/842412/covers/cover.jpg"
+          }
+          alt="cover"
+        />
+      </div>
+    </div>
   );
 };
 
